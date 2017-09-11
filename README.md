bd15c1cd63bc326eeaf4acb6162469dda827873d
Sam Gensburg <sam.gensburg@cms.hhs.gov>
2017-09-11 17:23:12 -0400

    Detailed setup instructions

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ It demonstrates the OAuth2 Server Side web application flow where a `client_secr
 
 ## Status and Contributing
 
-This application has been tested using Python 3.5.1 and Django 1.10, but should work just fine
-on Python 2.7 and 3.4. The application is in active development so check back often for updates.
+This application has been tested using Python 3.5.1 and Django 1.10.
+The application is in active development so check back often for updates.
 Please consider improving this code with your contributions. Pull requests welcome ;)
 
 ## Basic Setup
@@ -19,17 +19,44 @@ Please consider improving this code with your contributions. Pull requests welco
     python -m venv venv
     source venv/bin/activate
     pip install -r requirements/requirements.in
-    python manage.py migrate --settings bbc.settings.base
-    python manage.py runserver --settings bbc.settings.base
+    cp bbc/settings/local_sample.py bbc/settings/local.py
+    python manage.py migrate --settings bbc.settings.local
 
-## Set and Adjust your Settings.
+### Configuring Your Development Application
 
-You must register an client application and set settings values for:
+By default, your application will be set up to use the public OAuth service
+at https://dev.bluebutton.cms.fhirservice.net/. In order to use this version of
+the service, you'll need to request an account on that site. So select Account ->
+"Request an Invite," fill out the form, setting user type to "Developer," and
+we'll get back to you as soon as possible.
+
+Once you have your developer account created and you've verified your email address,
+you'll need to set up an application. Log in to your new account, and select
+"Applications" -> "Applications You Created" -> "Register New Application". From
+here, you can fill out the form with the following options:
+
+    Scope: [at least one option]
+    Name: [your choice]
+    Client type: Confidential
+    Authorization grant type: Authorization code
+    Redirect uris: http://localhost:8000/social-auth/complete/oauth2io
+
+Once you submit the form, you should receive an application key and secret that
+can be be added to the bbc/settings/local.py file you created above, overwriting
+the values for:
 
   * `SOCIAL_AUTH_OAUTH2IO_KEY`
   * `SOCIAL_AUTH_OAUTH2IO_SECRET`
 
-You can override the host with which your are communicating by adjusting these settings:
+### Final Steps
+
+Finally, you're ready to execute
+
+    python manage.py runserver --settings bbc.settings.local
+
+And from here, you can navigate to http://localhost:8000 and test your application.
+
+## Other Settings
 
   *  `OAUTH2IO_HOST`   - the default is `https://dev.bluebutton.cms.fhirservice.net`
   *  `EXTERNAL_AUTH_NAME` - the default is `CMS`.
--- bbc/bbc/settings/local_sample.py
+++ bbc/bbc/settings/local_sample.py
@@ -2,12 +2,99 @@ from .base import *
 import os
 
 
+
+"""
+Django settings for bbc project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.9/ref/settings/
+"""
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+from django.contrib.messages import constants as messages
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.join(BASE_DIR, '..')
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'piehme*+^#hylq8uz2eszps%o!5!+*#1@+*83gmp$o(u3%!ldp'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# This allows oauth to operate over http. DO NOT USE THIS CONFIG IN PRODUCTION
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+ALLOWED_HOSTS = ['*', ]
 
-SECRET_KEY = "BBCLIENT-LOCAL-_cdlv24!g$4)b&wq9fjn)p!vrs729idssk2qp7iy!u#!"
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'bootstrapform',
+    'apps.accounts',
+    'apps.home',
+    'apps.patient',
+    'social_django',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
+)
+ROOT_URLCONF = 'bbc.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
+                'django_settings_export.settings_export',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'bbc.wsgi.application'
+
+
+MESSAGE_TAGS = {messages.DEBUG: 'debug',
+                messages.INFO: 'info',
+                messages.SUCCESS: 'success',
+                messages.WARNING: 'warning',
+                messages.ERROR: 'danger'
+                }
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DBPATH = os.path.join(BASE_DIR, 'db/db.db')
 DATABASES = {
@@ -25,11 +112,86 @@ DATABASES = {
     }
 }
 
-# Set these values for you environment
-SOCIAL_AUTH_OAUTH2IO_KEY = ''
-SOCIAL_AUTH_OAUTH2IO_SECRET = ''
-SOCIAL_AUTH_OAUTH2IO_SCOPE = []
-OAUTH2IO_HOST = "http://example.com"
-OAUTH2_PROVIDER_NAME = "Your OAuth2"
-APP_TITLE = "Your Client Example"
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
+
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'sitestatic'),
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+
+AUTH_PROFILE_MODULE = 'accounts.UserProfile'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'apps.accounts.oauth_backends.oauth2_io.OAuth2ioOAuth2')
+
+
+# python-social-auth settings
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+SOCIAL_AUTH_OAUTH2IO_KEY = '' # Fill this out
+SOCIAL_AUTH_OAUTH2IO_SECRET = '' # Fill this out
+SOCIAL_AUTH_OAUTH2IO_SCOPE = []
+OAUTH2IO_HOST = "https://dev.bluebutton.cms.fhirservice.net"
+OAUTH2_PROVIDER_NAME = "CMS"
+APP_TITLE = "Blue Button Client Example"
+SOCIAL_AUTH_ALWAYS_ASSOCIATE = True
+# the trailing slash is necessary, because python-social-auth does not follow
+# redirects by default.
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGIN_ERROR_URL = '/accounts/login-error'
+HOSTNAME_URL = "http://bbc:8001"
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.mail.mail_validation',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.debug.debug',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'social_core.pipeline.debug.debug'
+)
+
+
+SETTINGS_EXPORT = [
+    'OAUTH2_PROVIDER_NAME',
+    'OAUTH2IO_HOST',
+    'APP_TITLE'
+]
