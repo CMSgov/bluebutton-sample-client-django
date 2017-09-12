b0da1100f9cb5e138d1b7ae52cd4c51a9a7667e2
Alan Viars <aviars@videntity.com>
2017-09-12 16:12:12 -0400

    fixed key length. Added more comments and option examples.

--- bbc/bbc/settings/local_sample.py
+++ bbc/bbc/settings/local_sample.py
@@ -1,10 +1,14 @@
 from .base import *
-import os
+# import os
 
 
 DEBUG = True
 
-SECRET_KEY = "This must be a secret"
+# change this key for your environment
+SECRET_KEY = "change-meiva8i*!ox)d(919hiy8zecc3&q)*=bophl@kzw05gick(&$wb7"
+
+# Uncomment the next line and "import os" to operate over http. DO NOT in production.
+# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 # Set these values for you environment
 SOCIAL_AUTH_OAUTH2IO_KEY = ''
@@ -12,3 +16,6 @@ SOCIAL_AUTH_OAUTH2IO_SECRET = ''
 SOCIAL_AUTH_OAUTH2IO_SCOPE = []
 OAUTH2_PROVIDER_NAME = "Your OAuth2"
 APP_TITLE = "Sample Application Title"
+
+# Uncomment the following line to point to another authorization service.
+# OAUTH2IO_HOST = "http://localhost:8000"
