6e5ae40190178709fd8aad7321d18e28667162a8
Bo Lopker <blopker@23andme.com>
2018-02-22 14:19:18 -0800

    Don't use Django 2

--- bbc/apps/patient/views.py
+++ bbc/apps/patient/views.py
@@ -16,38 +16,39 @@ from collections import OrderedDict
 import requests
 from requests_oauthlib import OAuth2
 
-
 __author__ = "Alan Viars"
 
 
 def build_fhir_urls(patient_id=None, since_date=None):
     result = {}
-    base = getattr( settings, 'FHIR_BASE_ENDPOINT',
-            "https://sandbox.bluebutton.cms.gov/v1/fhir/")
+    base = getattr(settings, 'FHIR_BASE_ENDPOINT',
+                   "https://sandbox.bluebutton.cms.gov/v1/fhir/")
     eob_search = urljoin(base, "ExplanationOfBenefit/")
     result['eob_search'] = eob_search
     patient_search = urljoin(base, "Patient/")
     result['patient_search'] = patient_search
 
     if patient_id:
-        result['coverage'] = urljoin(base, "Coverage/?beneficiary=%s" % (patient_id))
-        result['eob'] = urljoin(base, "ExplanationOfBenefit/?patient=%s" % (patient_id))
+        result['coverage'] = urljoin(base,
+                                     "Coverage/?beneficiary=%s" % (patient_id))
+        result['eob'] = urljoin(base, "ExplanationOfBenefit/?patient=%s" %
+                                (patient_id))
         result['patient'] = urljoin(base, "Patient/%s" % (patient_id))
     return result
-
-
-
-
 
 
 @login_required
 def bbof_get_userinfo(request):
     context = {'name': 'Get Userinfo'}
-    url = getattr( settings, 'USER_INFO_ENDPOINT',
-            "https://sandbox.bluebutton.cms.gov/v1/connect/userinfo")
+    url = getattr(settings, 'USER_INFO_ENDPOINT',
+                  "https://sandbox.bluebutton.cms.gov/v1/connect/userinfo")
     token = request.user.social_auth.get(provider='oauth2io').access_token
-    auth = OAuth2(settings.SOCIAL_AUTH_OAUTH2IO_KEY,
-                  token={'access_token': token, 'token_type': 'Bearer'})
+    auth = OAuth2(
+        settings.SOCIAL_AUTH_OAUTH2IO_KEY,
+        token={
+            'access_token': token,
+            'token_type': 'Bearer'
+        })
     response = requests.get(url, auth=auth)
 
     if response.status_code in (200, 404):
@@ -70,12 +71,16 @@ def bbof_get_userinfo(request):
 @login_required
 def bbof_get_patient(request, patient_id=None):
     context = {'name': 'Blue Button on FHIR'}
-    urls =  build_fhir_urls(patient_id)
-    
+    urls = build_fhir_urls(patient_id)
+
     # first we get the token used to login
     token = request.user.social_auth.get(provider='oauth2io').access_token
-    auth = OAuth2(settings.SOCIAL_AUTH_OAUTH2IO_KEY,
-                  token={'access_token': token, 'token_type': 'Bearer'})
+    auth = OAuth2(
+        settings.SOCIAL_AUTH_OAUTH2IO_KEY,
+        token={
+            'access_token': token,
+            'token_type': 'Bearer'
+        })
     # next we call the remote api
     url = urls['patient']
 
@@ -101,17 +106,18 @@ def bbof_get_patient(request, patient_id=None):
     return render(request, 'bbof.html', context)
 
 
-
-
-
 @login_required
 def bbof_get_eob(request, patient_id=None):
     context = {'name': 'Blue Button on FHIR'}
     # first we get the token used to login
     token = request.user.social_auth.get(provider='oauth2io').access_token
-    auth = OAuth2(settings.SOCIAL_AUTH_OAUTH2IO_KEY,
-                  token={'access_token': token, 'token_type': 'Bearer'})
-    urls =  build_fhir_urls(patient_id)
+    auth = OAuth2(
+        settings.SOCIAL_AUTH_OAUTH2IO_KEY,
+        token={
+            'access_token': token,
+            'token_type': 'Bearer'
+        })
+    urls = build_fhir_urls(patient_id)
     response = requests.get(urls['eob'], auth=auth)
 
     if response.status_code in (200, 404):
@@ -139,9 +145,13 @@ def bbof_get_coverage(request, patient_id=None):
     context = {'name': 'Blue Button on FHIR'}
     # first we get the token used to login
     token = request.user.social_auth.get(provider='oauth2io').access_token
-    auth = OAuth2(settings.SOCIAL_AUTH_OAUTH2IO_KEY,
-                  token={'access_token': token, 'token_type': 'Bearer'})
-    urls =  build_fhir_urls(patient_id)
+    auth = OAuth2(
+        settings.SOCIAL_AUTH_OAUTH2IO_KEY,
+        token={
+            'access_token': token,
+            'token_type': 'Bearer'
+        })
+    urls = build_fhir_urls(patient_id)
     response = requests.get(urls['coverage'], auth=auth)
 
     if response.status_code in (200, 404):
@@ -160,28 +170,24 @@ def bbof_get_coverage(request, patient_id=None):
     return render(request, 'bbof.html', context)
 
 
-
-
-
 @login_required
 def bbof_get_fhir(request, resource_type, patient):
     context = {'name': 'Blue Button on FHIR'}
     # first we get the token used to login
     token = request.user.social_auth.get(provider='oauth2io').access_token
-    auth = OAuth2(settings.SOCIAL_AUTH_OAUTH2IO_KEY,
-                  
-                  
-                  
-                  token={'access_token': token, 'token_type': 'Bearer'})
+    auth = OAuth2(
+        settings.SOCIAL_AUTH_OAUTH2IO_KEY,
+        token={
+            'access_token': token,
+            'token_type': 'Bearer'
+        })
     # next we call the remote api
     endpoint = '/v1/fhir/%s/?%s=59b99cd030c49e0001481f44&_format=json' % (
         resource_type, patient)
 
     url = urljoin(
-        getattr(
-            settings,
-            'OAUTH2IO_HOST',
-            "https://sandbox.bluebutton.cms.gov"), endpoint)
+        getattr(settings, 'OAUTH2IO_HOST',
+                "https://sandbox.bluebutton.cms.gov"), endpoint)
     print(url)
     # % (request.user.username)
 
@@ -198,16 +204,15 @@ def bbof_get_fhir(request, resource_type, patient):
         content = {'error': 'No read capability'}
         content = response.content
     elif response.status_code <= 500:
-        content = {'error': '500 Error from the server.',
-                   'status_code': response.status_code }
+        content = {
+            'error': '500 Error from the server.',
+            'status_code': response.status_code
+        }
         content = response.content
     else:
         content = response.content
 
     context['remote_status_code'] = response.status_code
     context['remote_content'] = content
-    ontext['patient'] = patient
+    context['patient'] = patient
     return render(request, 'bbof.html', context)
-
-
-
