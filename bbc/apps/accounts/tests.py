d9ba53652f47b62d389594d1b4fdc315953fea65
Mark Scrimshire <mscrimshire+github@gmail.com>
2018-10-16 14:59:43 -0400

    Setting up tests

--- bbc/apps/accounts/tests.py
+++ bbc/apps/accounts/tests.py
@@ -1,65 +1,65 @@
-from __future__ import unicode_literals
-from __future__ import absolute_import
-
-from unittest import skipIf
-
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
-
-from django.test import TestCase, override_settings  # noqa
-
-from apps.test import BaseApiTest
-
-from .oauth_backends.myoauth import MyOAuthOAuth2
-
-
-def user_profile_undefined():
-    """
-    Helper function that returns true when MY_USER_PROFILE_URL
-    setting is undefined or empty.
-    """
-    return not getattr(settings, 'MY_USER_PROFILE_URL', None)
-
-
-def no_test_server_credentials():
-    """
-    Helper function that returns true when TEST_INTEGRATION_*
-    credentials are undefined or empty.
-    """
-    client_id = getattr(settings, 'TEST_INTEGRATION_CLIENT_ID', None)
-    username = getattr(settings, 'TEST_INTEGRATION_USERNAME', None)
-    password = getattr(settings, 'TEST_INTEGRATION_PASSWORD', None)
-    return not (client_id and username and password)
-
-
-class TestMyOAuthBackend(BaseApiTest):
-    @skipIf(user_profile_undefined(), "MY_USER_PROFILE_URL setting is empty or undefined")
-    @override_settings(MY_USER_PROFILE_URL=None)
-    def test_get_user_profile_url_raise_exception(self):
-        backend = MyOAuthOAuth2()
-        with self.assertRaises(ImproperlyConfigured):
-            backend.get_user_profile_url()
-
-    @skipIf(user_profile_undefined(), "MY_USER_PROFILE_URL setting is empty or undefined")
-    def test_get_user_profile_url(self):
-        url = MyOAuthOAuth2().get_user_profile_url()
-        self.assertEqual(url, settings.MY_USER_PROFILE_URL)
-
-    @skipIf(no_test_server_credentials(), "TEST_INTEGRATION_* settings are empty or undefined")
-    def test_user_data(self):
-        """
-        Tests that the server return a json with the expected schema and content.
-        """
-        # get a valid access token from the server
-        access_token = self._get_test_access_token(settings.TEST_INTEGRATION_CLIENT_ID)
-        # call oauth server to retrieve user data
-        # the server should define an user with the following profile info
-        from social.strategies.django_strategy import DjangoStrategy
-        from social.storage.django_orm import BaseDjangoStorage
-        user_data = MyOAuthOAuth2(DjangoStrategy(BaseDjangoStorage)).user_data(access_token)
-        self.assertDictContainsSubset({
-            'username': 'test_user',
-            'first_name': 'Test',
-            'last_name': 'User',
-            'email': 'test_user@a.com',
-        }, user_data)
+# from __future__ import unicode_literals
+# from __future__ import absolute_import
+#
+# from unittest import skipIf
+#
+# from django.conf import settings
+# from django.core.exceptions import ImproperlyConfigured
+#
+# from django.test import TestCase, override_settings
+#
+# from apps.test import BaseApiTest
+#
+# from .oauth_backends.oauth2_io import OAuth2ioOAuth2
+#
+#
+# def user_profile_undefined():
+#     """
+#     Helper function that returns true when MY_USER_PROFILE_URL
+#     setting is undefined or empty.
+#     """
+#     return not getattr(settings, 'MY_USER_PROFILE_URL', None)
+#
+#
+# def no_test_server_credentials():
+#     """
+#     Helper function that returns true when TEST_INTEGRATION_*
+#     credentials are undefined or empty.
+#     """
+#     client_id = getattr(settings, 'TEST_INTEGRATION_CLIENT_ID', None)
+#     username = getattr(settings, 'TEST_INTEGRATION_USERNAME', None)
+#     password = getattr(settings, 'TEST_INTEGRATION_PASSWORD', None)
+#     return not (client_id and username and password)
+#
+#
+# class TestMyOAuthBackend(BaseApiTest):
+#     @skipIf(user_profile_undefined(), "MY_USER_PROFILE_URL setting is empty or undefined")
+#     @override_settings(MY_USER_PROFILE_URL=None)
+#     def test_get_user_profile_url_raise_exception(self):
+#         backend = OAuth2ioOAuth2()
+#         with self.assertRaises(ImproperlyConfigured):
+#             backend.get_user_profile_url()
+#
+#     @skipIf(user_profile_undefined(), "MY_USER_PROFILE_URL setting is empty or undefined")
+#     def test_get_user_profile_url(self):
+#         url = OAuth2ioOAuth2().get_user_profile_url()
+#         self.assertEqual(url, settings.MY_USER_PROFILE_URL)
+#
+#     @skipIf(no_test_server_credentials(), "TEST_INTEGRATION_* settings are empty or undefined")
+#     def test_user_data(self):
+#         """
+#         Tests that the server return a json with the expected schema and content.
+#         """
+#         # get a valid access token from the server
+#         access_token = self._get_test_access_token(settings.TEST_INTEGRATION_CLIENT_ID)
+#         # call oauth server to retrieve user data
+#         # the server should define an user with the following profile info
+#         from social.strategies.django_strategy import DjangoStrategy
+#         from social.storage.django_orm import BaseDjangoStorage
+#         user_data = OAuth2ioOAuth2(DjangoStrategy(BaseDjangoStorage)).user_data(access_token)
+#         self.assertDictContainsSubset({
+#             'username': 'test_user',
+#             'first_name': 'Test',
+#             'last_name': 'User',
+#             'email': 'test_user@a.com',
+#         }, user_data)
--- bbc/apps/test.py
+++ bbc/apps/test.py
@@ -1,16 +1,16 @@
-# from __future__ import absolute_import
-# from __future__ import unicode_literals
-#
-# from django.test.client import Client
-# from django.conf import settings
-# from django.contrib.auth.models import User
-# from django.test import TestCase
-# from .accounts.oauth_backends.oauth2_io import OAuth2ioOAuth2
-# from django.core.serializers.json import DjangoJSONEncoder
-# from unittest import skipUnless, skip
-# from requests_oauthlib import OAuth2Session
-#
-#
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from django.test.client import Client
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.test import TestCase
+from .accounts.oauth_backends.oauth2_io import OAuth2ioOAuth2
+from django.core.serializers.json import DjangoJSONEncoder
+from unittest import skipUnless, skip
+from requests_oauthlib import OAuth2Session
+
+
 # ENCODED = settings.ENCODING
 #
 #
@@ -24,7 +24,6 @@
 #     #     return True
 #     # print("No connection to:", host)
 #     # return False
-#
 #
 #
 # class BlueButtonApiTest(TestCase):
@@ -81,46 +80,46 @@
 #
 #
 #
-# class BaseApiTest(TestCase):
-#     """
-#     This class contains some helper methods useful to test API endpoints
-#     protected with oauth2 using DOT.
-#     """
-#
-#     def setUp(self):
-#         backend = OAuth2ioOAuth2()
-#         self.token_url = backend.ACCESS_TOKEN_URL
-#
-#     def _create_user(self, username, password, **extra_fields):
-#         """
-#         Helper method that creates a user instance
-#         with `username` and `password` set.
-#         """
-#         user = User.objects.create_user(
-#             username, password=password, **extra_fields)
-#         return user
-#
-#     #@skipUnless(can_reach_server(), "Requires access to server.")
-#     # def _get_test_access_token(self, client_id):
-#     #     """
-#     #     Helper method that returns an access token from the test server.
-#     #     """
-#     #     print("hello")
-#     #
-#     #     from oauthlib.oauth2 import LegacyApplicationClient
-#     #     from requests_oauthlib import OAuth2Session
-#     #     # get credentials for the test server
-#     #     username = getattr(settings, 'TEST_INTEGRATION_USERNAME', None)
-#     #     password = getattr(settings, 'TEST_INTEGRATION_PASSWORD', None)
-#     #
-#     #     print("clientid", client_id)
-#     #     # get the access token with password flow
-#     #     oauth = OAuth2Session(
-#     #         client=LegacyApplicationClient(client_id=client_id))
-#     #     token = oauth.fetch_token(
-#     #         token_url=self.token_url,
-#     #         username=username, password=password, client_id=client_id)
-#     #     return token.get('access_token')
-#
-#
-#
+class BaseApiTest(TestCase):
+    """
+    This class contains some helper methods useful to test API endpoints
+    protected with oauth2 using DOT.
+    """
+
+    def setUp(self):
+        backend = OAuth2ioOAuth2()
+        self.token_url = backend.ACCESS_TOKEN_URL
+
+    def _create_user(self, username, password, **extra_fields):
+        """
+        Helper method that creates a user instance
+        with `username` and `password` set.
+        """
+        user = User.objects.create_user(
+            username, password=password, **extra_fields)
+        return user
+
+    #@skipUnless(can_reach_server(), "Requires access to server.")
+    # def _get_test_access_token(self, client_id):
+    #     """
+    #     Helper method that returns an access token from the test server.
+    #     """
+    #     print("hello")
+    #
+    #     from oauthlib.oauth2 import LegacyApplicationClient
+    #     from requests_oauthlib import OAuth2Session
+    #     # get credentials for the test server
+    #     username = getattr(settings, 'TEST_INTEGRATION_USERNAME', None)
+    #     password = getattr(settings, 'TEST_INTEGRATION_PASSWORD', None)
+    #
+    #     print("clientid", client_id)
+    #     # get the access token with password flow
+    #     oauth = OAuth2Session(
+    #         client=LegacyApplicationClient(client_id=client_id))
+    #     token = oauth.fetch_token(
+    #         token_url=self.token_url,
+    #         username=username, password=password, client_id=client_id)
+    #     return token.get('access_token')
+
+
+
