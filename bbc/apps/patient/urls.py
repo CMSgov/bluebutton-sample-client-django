a0459df4c1ae6b120369c5369386512e1a48ec29
Dave Tisza <dtisza@fearless.tech>
2019-04-05 15:52:08 -0400

    Remove bbof_get_fhir and add metadata

--- bbc/apps/patient/urls.py
+++ bbc/apps/patient/urls.py
@@ -1,16 +1,12 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from django.conf.urls import url
-from .views import (bbof_get_patient, bbof_get_eob, bbof_get_coverage,
-                    bbof_get_fhir)
+from .views import (bbof_get_patient, bbof_get_eob, bbof_get_coverage)
 
 urlpatterns = [
     url(r'^Patient/',
         bbof_get_patient,
         name="bbof_get_patient"),
-    url(r'^fhir-resource//(?P<fhir_resource>[^/]+)$',
-        bbof_get_fhir,
-        name="bbof_get_fhir"),
     url(r'^ExplanationOfBenefit/',
         bbof_get_eob,
         name="bbof_get_eob"),
